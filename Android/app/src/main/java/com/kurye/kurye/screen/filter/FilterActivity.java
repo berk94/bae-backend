6f4a9dabda67652d8badc23f35a6d690d05672e4
Ahmet Zorer <ahmetcj4@hotmail.com>
2018-02-17 18:50:23 +0300

    FilterActivity fab removed

--- Android/app/src/main/java/com/kurye/kurye/screen/filter/FilterActivity.java
+++ Android/app/src/main/java/com/kurye/kurye/screen/filter/FilterActivity.java
@@ -1,31 +1,25 @@
 package com.kurye.kurye.screen.filter;
 
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.view.View;
 
 import com.kurye.kurye.R;
 
 public class FilterActivity extends AppCompatActivity {
+    public static void start(Context context) {
+        Intent starter = new Intent(context, FilterActivity.class);
+        context.startActivity(starter);
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_filter);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
     }
 
 }
