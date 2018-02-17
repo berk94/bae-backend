9766f61b867771c714571176c68520a28d0eddf0
Kemal Berk Kocabagli <berk.kocabagli@boun.edu.tr>
2018-02-17 19:03:57 +0300

    fix bug

--- config/mongo.js
+++ config/mongo.js
@@ -1,8 +1,11 @@
 // look at env-example and fill env.js
-const env = require('./env');
+try {
+    const env = require('./env');
+} catch (ex) {
+    console.log("Could not find env file. Should be on server.");
+}
 var dbuser = process.env.MONGOUSER || env.MONGO.dbuser;
 var dbpassword = process.env.MONGOPASSWORD || env.MONGO.dbpassword;
-
 module.exports = {
   url : "mongodb://" + dbuser + ":" + dbpassword + "@" + env.MONGO.mlab_url,
   seedDB: true
