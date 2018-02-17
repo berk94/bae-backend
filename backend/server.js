db02b326fabc7dd538c3f3c6f352c84e70557f1e
Kemal Berk Kocabagli <berk.kocabagli@boun.edu.tr>
2018-02-17 16:32:06 +0300

    Add server.js

---
+++ backend/server.js
@@ -0,0 +1,33 @@
+"use strict";
+
+const express = require('express');
+const bodyParser = require('body-parser'); // for handling json
+
+const app = express();
+const DEFAULT_PORT=3000;
+const mongo = require('./config/mongo');
+
+// Use bodyParser to parse requests in different formats
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+const mongoose = require('mongoose');
+mongoose.connect(mongo.url);
+
+const db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', function() {
+  //connected
+  // Populate DB with sample data
+  // if(mongo.seedDB){
+  //   require('./config/populate_db');
+  // }
+
+  require('./app/index')(app, db);
+
+  let PORT = process.env.PORT || DEFAULT_PORT
+  app.listen(PORT, () => {
+    console.log("Listening to port " + PORT + "...");
+  });
+
+});
