5f4a782020cd3764a4c09f59de90725d4487da14
Kemal Berk Kocabagli <berk.kocabagli@boun.edu.tr>
2018-02-17 17:46:03 +0300

    switched mongoDB to remote from local using mlab

---
+++ backend/config/env-example.js
@@ -0,0 +1,9 @@
+// environment variables for the app
+// fill these in.
+module.exports = {
+    'MONGO':{
+      'dbuser': 'enter username here',
+      'dbpassword': 'enter user password here',
+      'mlab_url': 'enter url here'
+    }
+};
--- backend/config/mongo.js
+++ backend/config/mongo.js
@@ -1,4 +1,7 @@
+// look at env-example and fill env.js
+const env = require('./env');
+
 module.exports = {
-  url : "mongodb://localhost:27017/bae-backend",
+  url : "mongodb://" + env.MONGO.dbuser + ":" + env.MONGO.dbpassword + "@" + env.MONGO.mlab_url,
   seedDB: true
 };
