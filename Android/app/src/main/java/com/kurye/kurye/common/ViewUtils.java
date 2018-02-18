8838f12196c7abca874b33cc61d6fbfbf480a2c3
Ahmet Zorer <ahmetcj4@hotmail.com>
2018-02-18 09:06:51 +0300

    snackbar duration and visibility updated

--- Android/app/src/main/java/com/kurye/kurye/common/ViewUtils.java
+++ Android/app/src/main/java/com/kurye/kurye/common/ViewUtils.java
@@ -8,14 +8,7 @@ import android.view.View;
 
 public class ViewUtils {
     public static Snackbar getSnackbar(Activity activity, String message) {
-        int marginBottom = SizeUtils.getNavigationBarSize(activity).y;
-        Snackbar snackbar = Snackbar.make(activity.findViewById(android.R.id.content), message, Snackbar.LENGTH_SHORT);
-        final View snackBarView = snackbar.getView();
-        snackBarView.setPadding(
-                snackBarView.getPaddingLeft(),
-                snackBarView.getPaddingTop(), snackBarView.getPaddingRight(),
-                snackBarView.getPaddingBottom() + marginBottom);
-        return snackbar;
+        return Snackbar.make(activity.findViewById(android.R.id.content), message, Snackbar.LENGTH_LONG);
     }
     public static Snackbar getSnackbarWithAction(Activity activity, String message,String actionTitle, View.OnClickListener listener) {
         return getSnackbar(activity,message).setAction(actionTitle, listener);
